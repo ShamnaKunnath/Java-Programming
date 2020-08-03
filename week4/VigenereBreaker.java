3c0fc6abb788307766b85987f4b422ba85954487
shamna123 <shamnakunnath54@gmail.com>
2020-08-03 22:23:30 +0530

    breakVigenere:UnknownKeyLength added added

--- week4/VigenereBreaker.java
+++ week4/VigenereBreaker.java
@@ -21,19 +21,63 @@ public class VigenereBreaker {
         }
         return key;
     }
-
+    public HashSet readDictionary(FileResource fr){
+        HashSet<String> h=new HashSet<String>();
+        for(String line: fr.lines()){
+            line=line.toLowerCase();
+            h.add(line);
+        }
+        return h;
+    }
+    public int countWords(String message, HashSet<String> dictionary){
+        String[] s=message.split("\\W");
+        int c=0;
+        for(int i=0;i<s.length;i++){
+            
+            if(dictionary.contains(s[i].toLowerCase())){
+                c++;
+            }
+        }
+        return c;
+    }
+    public String breakForLanguage(String encrypted,HashSet<String> dictionary){
+        int max=0;
+        int res=0;
+        String ans="";
+        for(int i=1;i<101;i++){
+            int[] key=tryKeyLength(encrypted,i,'e');
+            VigenereCipher vc=new VigenereCipher(key);
+            String dmsg=vc.decrypt(encrypted);
+            int count=countWords(dmsg,dictionary);
+            if(count>max){
+                max=count;
+                ans=dmsg;
+                res=key.length;
+            }
+        }
+        System.out.println("key: "+res+"valid words: "+max);
+        
+        return ans;
+    }
     public void breakVigenere () {
         //WRITE YOUR CODE HERE
-        FileResource fr=new FileResource("Data/athens_keyflute.txt");
+        FileResource fr=new FileResource("messages/secretmessage2.txt");
         String str=fr.asString();
-        int keylength=5;
-        int[] key=tryKeyLength(str, 5,'e');
+        FileResource english=new FileResource("dictionaries/English");
+        HashSet h=readDictionary(english);
+        //int keylength=4;
+        //int[] key=tryKeyLength(str, 4,'e');
+        //VigenereCipher vc=new VigenereCipher(key);
+        System.out.println(breakForLanguage(str,h).substring(0,100));
+        int[] key=tryKeyLength(str,38,'e');
         VigenereCipher vc=new VigenereCipher(key);
-        System.out.println(vc.decrypt(str));
+        String dmsg=vc.decrypt(str);
+        int count=countWords(dmsg,h);
+        System.out.println("no of valid words: "+count); 
     }
     public void tester(){
-        FileResource fr=new FileResource("Data/athens_keyflute.txt");
-        int[] key =tryKeyLength(fr.asString(),5,'e');
+        FileResource fr=new FileResource("messages/secretmessage1.txt");
+        int[] key =tryKeyLength(fr.asString(),4,'e');
         System.out.print("[");
         for(int i=0;i<key.length;i++){
            System.out.print(key[i]+", "); 
